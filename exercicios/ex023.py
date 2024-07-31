8cbb9f753e51aff73493c3ebee37948dde0e2b9f
carlosmacarao <carlosmacarao9@gmail.com>
2024-07-31 11:33:55 +0200

    Exercise 23

--- exercicios/ex023.py
+++ exercicios/ex023.py
@@ -1,6 +1,9 @@
 def analisa_numero():
     num = input('Informe um numero: ')
     
-    print(f'{num[2]}')
+    print(f'Unidade: {num[-1]}')
+    print(f'Dezena: {num[-2]}')
+    print(f'Centena: {num[1]}')
+    print(f'Milhar: {num[0]}')
 
 analisa_numero()    
